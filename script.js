1a31f8affb8dc24693ada783c09d9d2b322e0718
pedro3115 <64373231+pedro3115@users.noreply.github.com>
2020-05-21 11:58:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 $(function() {
 
+  var $navi = $('#navigation');
+
   $('#menu-icon').click(function(){
-    $('#sub-menu-bar').show();
+    if($navi.hasClass('open')) {
+      $navi.removeClass('open');
+      $navi.slideUp();
+    }
+    else {
+      $navi.addClass('open');
+      $navi.slideDown();
+    }
   });
 
   $('#main').click(function(){
-    $('#sub-menu-bar').hide();
+    if($navi.hasClass('open')) {
+      $navi.removeClass('open');
+      $navi.slideUp();
+    }
+  });
+
+  var swiper = new Swiper('.swiper-container', {
+    loop:true,
+    speed:1200,
+    touchRatio:2,
+    longSwipesRatio:0.3,
+    disableOnInteraction:false,
+    pagination: {
+		  el: '.swiper-pagination',
+		  type: 'bullets',
+		  clickable: true
+	  },
+    autoplay:{
+      delay:3000,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
   });
 
 });
